e2bc4ecbfea67f5dcf909bfe0ef58a6e108ecbcb
Neri D <nerissadorlus@gmail.com>
2022-10-11 11:23:48 -0400

    added async await example

--- async.js
+++ async.js
@@ -1,9 +1,19 @@
 import fs from 'fs';
 
 const filename = 'file.txt';
-let data = fs.readFile(filename, 'utf8', function (err, data) {
+//callback
+fs.readFile(filename, 'utf8', function (err, data) {
   console.log(data);
 });
-data = fs.readFile(filename, 'utf8').then((err, data) => {
+
+//promise/.then
+fs.readFile(filename, 'utf8').then((err, data) => {
   console.log(data);
 });
+
+//async
+async function showText() {
+  const data = await fs.readFileSync(filename, 'utf8');
+  return data;
+}
+console.log(showText());
